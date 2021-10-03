6ddd7020cd10562ad1d71762735c502441043254
Sam Harrison <samh99@outlook.com>
2021-10-03 15:23:07 +0100

    Class Roster Java Project

---
+++ ClassRoster/src/main/java/com/sg/classroster/dto/Student.java
@@ -0,0 +1,82 @@
+package com.sg.classroster.dto;
+
+import java.util.Objects;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String studentId;
+    // Programming Language + cohort month/year
+    private String cohort;
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 29 * hash + Objects.hashCode(this.firstName);
+        hash = 29 * hash + Objects.hashCode(this.lastName);
+        hash = 29 * hash + Objects.hashCode(this.studentId);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Student other = (Student) obj;
+        if (!Objects.equals(this.firstName, other.firstName)) {
+            return false;
+        }
+        if (!Objects.equals(this.lastName, other.lastName)) {
+            return false;
+        }
+        if (!Objects.equals(this.studentId, other.studentId)) {
+            return false;
+        }
+        return Objects.equals(this.cohort, other.cohort);
+    }
+
+    public Student(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getCohort() {
+        return cohort;
+    }
+
+    public void setCohort(String cohort) {
+        this.cohort = cohort;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "firstName=" + firstName + ", lastName=" + lastName + ", studentId=" + studentId + ", cohort=" + cohort + '}';
+    }
+
+}
